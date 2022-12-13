8f1f249084475dcaa6a0a95bbb5578e6915d5397
Oachi <ongsa.srp.mkm@gmail.com>
2022-12-13 15:10:39 +0700

    betterer ui

--- index.js
+++ index.js
@@ -4,14 +4,23 @@ const rateup = document.getElementById('rateup')
 const rateup2 = document.getElementById('rateup2')
 
 let t = 0
+let kmode = true
+let rateups = []
 
-
-rateup.addEventListener('click', () => {
-    rateup2.checked = false
+rateup.addEventListener('click', (e) => {
+    if (e.target.checked) {
+        rateups.push(15)
+    } else {
+        rateups = rateups.filter(each => each!=15)
+    }
 })
 
-rateup2.addEventListener('click', () => {
-    rateup.checked = false
+rateup2.addEventListener('click', (e) => {
+    if (e.target.checked) {
+        rateups.push(17)
+    } else {
+        rateups = rateups.filter(each => each!=17)
+    }
 })
 
 document.addEventListener('keypress', e => {
@@ -34,49 +43,65 @@ document.addEventListener('keypress', e => {
     } else {
         t = 0
     }
-
 })
 
-const f = []
+const pool = []
 
 for (let i = 1; i <= 24; i++) {
-    f.push(i)
+    pool.push(i)
+}
+
+function choose(a) {
+    return a[Math.floor(Math.random() * a.length) % a.length]
 }
 
 function easeOutExpo(x) {
     return 1 - Math.sqrt(1 - Math.pow(x, 2));
-
 }
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-
 async function magic() {
     for (let i = 0; i / 30 <= 1; i++) {
         const wait = easeOutExpo(i / 30) ** 1.5 * 500;
-        console.log(i / 20, wait);
+        // console.log(i / 20, wait);
         await sleep(wait);
-        resultDisplay.innerText = Math.round(Math.random()*100) % 24 + 1
+        resultDisplay.innerText = choose(pool)
     }
 }
 
 async function doRandom() {
     await magic()
     if (t) {
+        if (kmode) {
+            resultDisplay.innerText = t
+            await sleep(800)
+            resultDisplay.innerText = choose(pool)
+            await sleep(500)
+        }
         resultDisplay.innerText = t
         // t = 0
     } else {
-        if (rateup.checked && Math.random() > 0.45) {
-            resultDisplay.innerText = 15
-            console.log('t')
-        } else if (rateup2.checked && Math.random() > 0.45) {
-            resultDisplay.innerText = 17
-        }
-        else {
-            const randomElement = f[Math.floor(Math.random() * f.length)]
-            resultDisplay.innerText = randomElement
+        if (rateups.length !== 0) {
+            console.log('Rate up enebled')
+            if (Math.random() > 0.45) { // if rateup then randomly choose between those
+                console.log('Choosing rate up value')
+                if (kmode) {
+                    resultDisplay.innerText = choose(rateups)
+                    await sleep(800)
+                    resultDisplay.innerText = choose(pool)
+                    await sleep(500)
+                }
+                resultDisplay.innerText = choose(rateups)
+            } else {
+                console.log('Failed to get rate up value')
+                resultDisplay.innerText = choose(pool)
+            }
+        } else {
+            console.log('Rate up disabled')
+            resultDisplay.innerText = choose(pool)
         }
     }
 }
